c5b83a0acfb7e9f9d986ec30bff7cbf3c3e77abd
Danny Hieber <dwhieb@gmail.com>
2020-02-27 17:10:56 -0600

    work on tests

--- src/models/Language.js
+++ src/models/Language.js
@@ -1,4 +1,5 @@
-import Model from '../core/Model.js';
+import Model           from '../core/Model.js';
+import MultiLangString from './MultiLangString.js';
 
 /**
  * A class representing a [DLx Language Object]{@link https://format.digitallinguistics.io/schemas/Language.html}
@@ -12,7 +13,11 @@ class Language extends Model {
    * @param {Object} [data={}] The data to use for this Language object. Data should be formatted according to the [DLx Data Format's guidelines for Language data]{@link https://format.digitallinguistics.io/schemas/Language.html}.
    */
   constructor(data = {}) {
+
     super(data);
+
+    this.name = new MultiLangString(this.name);
+
   }
 
 }
--- src/models/Language.test.js
+++ src/models/Language.test.js
@@ -1,4 +1,13 @@
-const { models: { Language } } = require(`../../test`);
+/* eslint-disable
+  max-nested-callbacks,
+*/
+
+const { models } = require(`../../test`);
+
+const {
+  MultiLangString,
+  Language,
+} = models;
 
 describe(`Language`, () => {
 
@@ -8,11 +17,50 @@ describe(`Language`, () => {
 
   describe(`Language.prototype.name`, () => {
 
-    it(`class: MultiLangString`);
+    it(`class: MultiLangString`, () => {
+      const lang = new Language();
+      expect(lang.name).toBeInstanceOf(MultiLangString);
+    });
 
-    it(`Success: good data`);
+    describe(`Success: good data`, () => {
 
-    it(`Success: set good data`);
+      it(`String`, () => {
+        const name = `Chitimacha`;
+        const lang = new Language({ name });
+        expect(lang.name.get(`eng`)).toBe(name);
+      });
+
+      it(`Object`, () => {
+
+        const name = {
+          eng: `Chitimacha`,
+          fra: `chitimacha`,
+        };
+
+        const lang = new Language({ name });
+
+        expect(lang.name.get(`eng`)).toBe(name.eng);
+        expect(lang.name.get(`fra`)).toBe(name.fra);
+
+      });
+
+    });
+
+    fit(`Success: set good data`, () => {
+
+      const name = {
+        eng: `Chitimacha`,
+        fra: `chitimacha`,
+      };
+
+      const lang = new Language;
+
+      lang.name = name;
+
+      expect(lang.name.get(`eng`)).toBe(name.eng);
+      expect(lang.name.get(`fra`)).toBe(name.fra);
+
+    });
 
     it(`Error: bad data`);
 
