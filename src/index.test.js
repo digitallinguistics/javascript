d0aa1025b3db5a4279d099ebdb8a671e12f781a3
Danny Hieber <dwhieb@gmail.com>
2020-02-27 00:21:13 -0600

    add tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,10 @@
-const { models } = require(`../test`);
+const { core, models } = require(`../test`);
 
 describe(`dlx`, () => {
+
+  it(`core`, () => {
+    expect(core).toBeInstanceOf(Object);
+  });
 
   it(`models`, () => {
     expect(models).toBeInstanceOf(Object);
