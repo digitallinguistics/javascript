35cec2e1fc133284cb50a00f58e743a5ff9fc48e
Danny Hieber <dwhieb@gmail.com>
2020-02-27 17:43:29 -0600

    working on defineProp function

--- src/core/utilities/defineProp.js
+++ src/core/utilities/defineProp.js
@@ -1,4 +1,17 @@
-function defineProp() {
+/**
+ * A factory method that defines a property on an object, and ensures that the value of that property is always an instance of the provided model.
+ * @memberof core.utilities
+ * @instance
+ * @param  {Object}   object   The object to add the property to
+ * @param  {String}   propName The name of the property to add
+ * @param  {Function} model    A constructor function. The value of the defined property will always be an instance of this constructor.
+ */
+function defineProp(object, propName, model) {
+
+  const PropModel = model;
+  const isDate    = PropModel === Date;
+  const isNum     = PropModel === Number;
+  const isString  = PropModel === String;
 
 }
 
