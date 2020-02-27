0005e2a6d155f77d8fadceacb55a0a06fba6b33a
Danny Hieber <dwhieb@gmail.com>
2020-02-27 11:38:48 -0600

    add MultiLangString test file

---
+++ src/models/MultiLangString.test.js
@@ -0,0 +1,9 @@
+const { models: { MultiLangString } } = require(`../../test`);
+
+describe(`MultiLangString`, () => {
+
+  it(`is a MultiLangString class`, () => {
+    expect(MultiLangString.name).toBe(`MultiLangString`);
+  });
+
+});
