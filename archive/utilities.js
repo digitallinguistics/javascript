2443788b2cd1dcd681d4168164d92fd837c08dc1
Danny Hieber <dwhieb@gmail.com>
2020-02-27 02:10:14 -0600

    clean up old Language model

--- archive/utilities.js
+++ archive/utilities.js
@@ -1,4 +1,7 @@
-/* eslint-disable */
+/* eslint-disable
+  max-classes-per-file,
+  max-statements,
+*/
 
 /**
  * A collection of utility functions
@@ -204,46 +207,6 @@ function checkProp(...args) {
   }
 
   return item;
-
-}
-
-/**
- * Creates a new DOM element, and sets its properties
- * @param  {String} type  The type of element to create (e.g. `div`, `p`)
- * @param  {Object} props An object of properties and their values to assign to the new element
- * @return {Object}       Returns the new DOM Node
- */
-function createElement(type, props = {}) {
-  return Object.assign(document.createElement(type), props);
-}
-
-/**
- * A generic debounce function, which only fires a function in specified intervals. Useful for listening to events that happen frequently and require rerendering or heavy operations.
- * Documentation here: https://davidwalsh.name/javascript-debounce-function
- * @param  {Function} func      The function to debounce
- * @param  {Number}   wait      The time to wait, in milliseconds, between firing the callback
- * @param  {Boolean}  immediate Whether to invoke the callback once before the wait timer begins
- * @return {Function}           Returns the debounced function to use
- */
-function debounce(func, wait = 250, immediate = false) {
-
-  let timeout;
-
-  return function(...args) {
-
-    const later = () => {
-      timeout = null;
-      if (!immediate) func.apply(this, args);
-    };
-
-    const callNow = immediate && !timeout;
-
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-
-    if (callNow) func.apply(this, args);
-
-  };
 
 }
 
@@ -457,74 +420,6 @@ function defineSet(object, propName, model, initialValue = new Set) {
 }
 
 /**
- * Returns an array of all the unique characters in a string. Works with both UTF-8 and UTF-16 encoding.
- * @param  {String} string The string to retrieve unique characters from
- * @return {Array}         Returns an array of the unique characters
- * @instance
- */
-function getUniqueChars(string) {
-
-  if (typeof string !== `string`) {
-    throw new TypeError(`The argument passed to "getUniqueChars" must be a string.`);
-  }
-
-  return Array.from(new Set(string));
-
-}
-
-/**
- * Filters an array of LexemeReference objects and returns only JSON-unique ones
- * @ignore
- */
-function getUniqueReferences(references = []) {
-
-  if (!Array.isArray(references)) {
-    throw new TypeError(`The references must be an Array.`);
-  }
-
-  const uniques = [];
-
-  const isEqual = (obj1, obj2) => {
-
-    const stringsEqual = (
-      obj1.lexicon === obj2.lexicon
-      && obj1.lexeme === obj2.lexeme
-      && obj1.relation === obj2.relation
-      && obj1.sense === obj2.sense
-    );
-
-    if (stringsEqual) {
-
-      if (obj1.variantType && obj2.variantType) {
-
-        return (
-          Object.entries(obj1.variantType).every(([key, val]) => val === obj2.variantType[key])
-          && Object.entries(obj2.variantType).every(([key, val]) => val === obj1.variantType[key])
-        );
-
-      } else if (obj1.variantType || obj2.variantType) {
-
-        return false;
-
-      }
-
-      return true;
-
-    }
-
-    return false;
-
-  };
-
-  references.forEach(ref => {
-    if (!uniques.some(obj => isEqual(obj, ref))) uniques.push(ref);
-  });
-
-  return uniques;
-
-}
-
-/**
  * Tests whether a string is a valid abbreviation according to the [DLx Abbreviation schema]{@link http://developer.digitallinguistics.io/spec/schemas/abbreviation.html}.
  * @param  {String} string The string to test
  * @return {Boolean}       Returns true if the string is a valid abbreviation, false otherwise
@@ -563,239 +458,19 @@ function isURL(string) {
   return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(string);
 }
 
-/**
- * Performs a series of substitutions on a string, replacing unwanted characters with the desired ones (or nothing, meaning the character is simply removed everywhere it occurs).
- * @param  {String} string        The string to sanitize
- * @param  {Object} substitutions An object whose attributes are the characters you wish to replace, and values are the characters you wish to replace them with
- * @return {String}               Returns the sanitized string, with substitutions made
- * @instance
- */
-function sanitize(string, substitutions) {
-  return transliterate(string, substitutions);
-}
-
-/**
- * A Sanitizer class that stores a set of substitutions for repeated use on different strings
- * @type {Function}
- * @instance
- * @prop {Object} substitutions An Object containing the substitutions to apply to each string
- */
-class Sanitizer {
-  /**
-   * Create a new Sanitizer function
-   * @param  {Object} substitutions The set of substitutions to make (see the `sanitize` method)
-   * @return {Function}             Returns a function which always sanitizes a string with the given set of substitutions
-   */
-  constructor(substitutions) {
-    this.substitutions = substitutions;
-    return string => sanitize(string, substitutions);
-  }
-}
-
-/**
- * Takes an Object and an Array of required properties, and returns a simplified version of the object with all empty Arrays, Strings, Sets, Maps, and basic Objects removed. This is useful when trying to reduce the footprint of the object when stringifying it.
- * @param  {Object} model           The Object to simplify
- * @param  {Array}  [required = []] A Array of required property names. These properties will not be removed from the object, even if they are empty
- * @return {Object}                 Returns a *new* object with the properties removed
- */
-function simplify(model, required = []) {
-
-  if (!(model instanceof Object)) {
-    throw new TypeError(`The "model" argument must be an Object.`);
-  }
-
-  if (!Array.isArray(required) || required.some(val => typeof val !== 'string')) {
-    throw new TypeError(`The "required" argument must be an Array of Strings.`);
-  }
-
-  const o = Object.assign({}, model);
-
-  for (const prop in model) {
-
-    if (!required.includes(prop)) {
-
-      if (
-        (Array.isArray(o[prop]) && !o[prop].length)
-        || ((o[prop] instanceof Set || o[prop] instanceof Map) && !o[prop].size)
-        || (o[prop] && o[prop].constructor === Object && !Object.keys(o[prop]).length)
-        || (typeof o[prop] !== 'number' && !o[prop])
-      ) {
-        delete o[prop];
-      }
-
-      if (o[prop] instanceof Map) {
-        o[prop].forEach((val, key) => {
-          if (typeof val === 'string' && !val.length) o[prop].delete(key);
-        });
-      }
-
-    }
-
-  }
-
-  return o;
-
-}
-
-/**
- * Removes punctuation from a string, and then tokenizes the string, returning an array of tokens.
- * @param  {String} string      The string to tokenize
- * @param  {Array}  delimiters  An array of characters to tokenize the string with
- * @param  {Array}  punctuation An array of punctuation characters to remove from the string
- * @return {Array}              Returns an array of tokens
- * @instance
- */
-function tokenize(string, delimiters, punctuation) {
-
-  // NB: Don`t use default parameters, since they don`t allow `null` to be passed.
-  const delims = delimiters || [`\\s`];
-  const punct  = punctuation || defaultPunctuation;
-
-  if (typeof string !== `string`) {
-    throw new TypeError(`The "string" argument must be a string.`);
-  }
-
-  if (!(Array.isArray(delims) && Array.isArray(punct))) {
-    throw new TypeError(`The "delimiters" and "punctuation" arguments must be arrays.`);
-  }
-
-  const substitutions = {};
-
-  punct.forEach(item => { substitutions[item] = ``; });
-
-  const depunctuated = sanitize(string, substitutions);
-  const pattern      = `[${delims.join(``)}]+`;
-  const regexp       = new RegExp(pattern, `gu`);
-
-  return depunctuated.split(regexp);
-
-}
-
-/**
- * A Tokenizer class that saves a set of delimiters and punctuation characters for repeated use in tokenizing strings
- * @type {Function}
- * @instance
- * @prop {Array} delimiters  An array of delimiters to tokenize with
- * @prop {Array} punctuation An array of punctuation characters to strip from the string
- */
-class Tokenizer {
-  /**
-   * Create a new Tokenizer
-   * @param  {Array}    delimiters  The Array of delimiters used by this Tokenizer
-   * @param  {Array}    punctuation The Array of punctuation characters used by this Tokenizer
-   * @return {Function}             Returns a tokenize function which tokenizes a string with the provided set of delimiters and punctuation characters
-   */
-  constructor(delimiters, punctuation) {
-    this.delimiters  = delimiters;
-    this.punctuation = punctuation;
-    return string => tokenize(string, this.delimiters, this.punctuation);
-  }
-}
-
-/**
- * Converts a string from one writing system to another.
- * @param {String} string         The string to transliterate.
- * @param {Object} substitutions  A hash of substitutions to make on the string. Each key in the object should be a string to replace, and its value should be the string to replace it with.
- * @return {String}               Returns the transliterated string
- * @instance
- */
-function transliterate(string = ``, substitutions = {}) {
-
-  if (typeof string !== `string`) {
-    throw new TypeError(`The first argument passed to "transliterate" must be a string.`);
-  }
-
-  if (!(
-    typeof substitutions === `object`
-    && Object.values(substitutions).every(val => typeof val === `string`)
-  )) {
-    throw new TypeError(`The second argument passed to "substitutions" must be an object whose attributes and values are strings.`);
-  }
-
-  const temps = new Map;
-  const subs  = new Map;
-  let str     = string;
-
-  // gets a random Unicode point from the geometric shapes block
-  const getRandomCodePoint = () => String.fromCodePoint(Math.floor(Math.random() * 95) + 9632);
-  const inputs = Object.keys(substitutions);
-
-  Object.entries(substitutions)
-  .sort(([a], [b]) => b.length - a.length)
-  .forEach(([input, replacement]) => {
-
-    // escape regex special characters
-    const escapedInput = input.replace(/[.*+?^${}()|[\]\\]/g, `\\$&`);
-    subs.set(escapedInput, replacement);
-
-    // check for feeding problem
-    if (inputs.includes(replacement)) {
-      // create a temporary substitution using characters from the geometric shapes Unicode block
-      let temp = getRandomCodePoint();
-      // make sure you haven't already used that code point, and generate a new temporary substitution if so
-      while (temps.has(temp)) temp = getRandomCodePoint();
-      temps.set(temp, replacement);
-      subs.set(escapedInput, temp);
-    }
-
-    // make the substitution using the temporary placeholder
-    const regexp = new RegExp(escapedInput, `gu`);
-    str = str.replace(regexp, subs.get(escapedInput));
-
-  });
-
-  // now replace the temporary placeholders with their proper values
-  temps.forEach((replacement, temp) => {
-    const regexp = new RegExp(temp, `gu`);
-    str = str.replace(regexp, replacement);
-  });
-
-  return str;
-
-}
-
-/**
- * A Transliterator class that saves a set of transliteration rules for repeated use
- * @type {Function}
- * @instance
- * @prop {Object} substitutions The set of substitution rules for this object. If these are modified, the transliteration function will be modified accordingly.
- */
-class Transliterator {
-  /**
-   * Create a new Transliterator
-   * @param  {Object}   substitutions An object containing the set of substitutions to make, where the key is the string to be replaced, and the value is the is the string to replace it with
-   * @return {Function}               Returns a transliterator function which transliterates a string with the provided set of substitutions
-   */
-  constructor(substitutions) {
-    this.substitutions = substitutions;
-    return string => transliterate(string, this.substitutions);
-  }
-}
-
-module.exports = {
+export {
   addAbbreviation,
   addType,
   addURL,
   aliasLanguage,
   aliasTranscription,
   checkProp,
-  createElement,
-  debounce,
   defineArray,
   defineEnumProp,
   defineProp,
   defineSet,
-  getUniqueChars,
-  getUniqueReferences,
   isAbbr,
   isDateString,
   isIterable,
   isURL,
-  sanitize,
-  Sanitizer,
-  simplify,
-  tokenize,
-  Tokenizer,
-  transliterate,
-  Transliterator,
 };
--- src/models/Language.js
+++ src/models/Language.js
@@ -2,9 +2,26 @@
  * @module
  */
 
+import Model from '../core/Model.js';
+
 /**
  * A class representing a language
+ * @extends Model
  */
-class Language {}
+class Language extends Model {
+
+  /**
+   * Create a new Language
+   * @param {Object} [data={}] The data to use for this Language object
+   */
+  constructor(data = {}) {
+
+    super();
+
+    Object.assign(this, data);
+
+  }
+
+}
 
 export default Language;
