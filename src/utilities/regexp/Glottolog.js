84a81032fa52cb163907e029af6e226f548f3295
Vade Kamenitsa-Hale <kamenitsahal@wisc.edu>
2020-03-17 11:35:01 -0500

    Create Glottolog.js Regexp

---
+++ src/utilities/regexp/Glottolog.js
@@ -0,0 +1,9 @@
+/**
+ * A regular expression for Glottolog Language Codes.
+ * @memberof utilities.regexp
+ * @instance
+ * @type {RegExp}
+ */
+const glottoCodeRegExp = /^[a-z]{4}[0-9]{4}$/u;
+
+export default glottoCodeRegExp;
