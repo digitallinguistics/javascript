0c7faaf0f6fa4d5684eff645be6fe66f99d566bf
Vade Kamenitsa-Hale <kamenitsahal@wisc.edu>
2020-03-17 12:01:17 -0500

    Add abbreviation regex

---
+++ src/utilities/regexp/Abbreviation.js
@@ -0,0 +1,9 @@
+/**
+ * A regular expression for language abbreviations.
+ * @memberof utilities.regexp
+ * @instance
+ * @type {RegExp}
+ */
+const abbreviationRegExp = /^[(a-z)|(A-Z)|(0-9)]$/u;
+
+export default abbreviationRegExp;
