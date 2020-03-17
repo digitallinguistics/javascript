8682084ccde8b632ec0353e446451e51342b7a78
Vade Kamenitsa-Hale <kamenitsahal@wisc.edu>
2020-03-17 12:01:37 -0500

    Add abbreviation regex validation

---
+++ src/utilities/validate/isAbbreviation.js
@@ -0,0 +1,14 @@
+import abbreviationRegExp from '../regexp/Abbreviation.js';
+
+/**
+ * Checks whether the input is a valid language abbreviation.
+ * @memberof utilities.validate
+ * @instance
+ * @param  {Any}     input The input to check
+ * @return {Boolean}
+ */
+function isAbbreviation(input) {
+  return abbreviationRegExp.test(input);
+}
+
+export default isAbbreviation;
