8e90751839663bf132bdb9dea7395cf20dc8155a
Daniel W. Hieber <dwhieb@gmail.com>
2020-08-16 18:56:30 -0500

    write skeleton tests

---
+++ src/core/Collection.test.js
@@ -0,0 +1,18 @@
+import chai       from 'chai';
+import Collection from './Collection.js';
+
+chai.should();
+
+describe(`Collection`, () => {
+
+  it(`is the Collection class`, () => {
+
+    Collection.name.should.equal(`Collection`);
+
+  });
+
+  it(`instantiates with all items as instances of the provided model`);
+
+  it(`transforms items to instances of the provided model when added`);
+
+});
