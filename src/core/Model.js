ee3846d1161035e455348ce9ceb3cf8596c80695
Daniel W. Hieber <dwhieb@gmail.com>
2020-08-16 18:13:03 -0500

    tests passing

--- src/core/Model.js
+++ src/core/Model.js
@@ -5,9 +5,26 @@
 class Model {
 
   /**
-   * Create a new Model
+   * A utility function to define a property on a class which must always be an instance of a certain class. **NOTE:** _This method may only be called inside a class constructor. The private property (`#propName`) must also be defined on the class._
+   * @param {Object}   object    The object to define the property on
+   * @param {String}   propName  The name of the property to define on the object
+   * @param {Function} ItemModel The class to use for this property. Every time this property is set, this model will be instantiated.
+   * @return {Object}            Returns the original object with the new property added
    */
-  constructor() {} // eslint-disable-line
+  static defineModelProp(object, propName, ItemModel) {
+
+    Object.defineProperty(object, propName, {
+      configurable: true,
+      enumerable:   true,
+      get() {
+        return this[`#${propName}`];
+      },
+      set(val) {
+        this[`#${propName}`] = new ItemModel(val);
+      },
+    });
+
+  }
 
 }
 
--- src/core/Model.test.js
+++ src/core/Model.test.js
@@ -9,4 +9,26 @@ describe(`Model`, () => {
     Model.name.should.equal(`Model`);
   });
 
+  it(`defineModelProp`, () => {
+
+    class TestModel {}
+
+    class TestObject {
+
+      #testProp;
+
+      constructor() {
+        Model.defineModelProp(this, `testProp`, TestModel);
+      }
+
+    }
+
+    const testObject = new TestObject;
+
+    testObject.testProp = true;
+
+    testObject.testProp.should.be.instanceOf(TestModel);
+
+  });
+
 });
