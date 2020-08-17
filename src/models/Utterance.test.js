2b72144c0c9078d7e56e57ff43e4bfea7a41bc84
Daniel W. Hieber <dwhieb@gmail.com>
2020-08-16 19:17:13 -0500

    write skeleton tests

---
+++ src/models/Utterance.test.js
@@ -0,0 +1,15 @@
+describe(`Utterance`, () => {
+
+  it(`transcript is a Transcription object`);
+
+  it(`transcript is undefined if absent`);
+
+  it(`transcription is a Transcription object`);
+
+  it(`transcription is an empty Map object if absent`);
+
+  it(`translation is a Translation object`);
+
+  it(`translation is an empty string in English if absent`);
+
+});
