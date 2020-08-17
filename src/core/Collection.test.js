f9d10e19b8843d0b25ba31e4bb88cb6365876446
Daniel W. Hieber <dwhieb@gmail.com>
2020-08-16 19:04:54 -0500

    tests passing

--- src/core/Collection.test.js
+++ src/core/Collection.test.js
@@ -5,14 +5,33 @@ chai.should();
 
 describe(`Collection`, () => {
 
-  it(`is the Collection class`, () => {
+  class ItemModel {}
 
+  it(`is the Collection class`, () => {
     Collection.name.should.equal(`Collection`);
+  });
+
+  it(`instantiates with all items as instances of the provided model`, () => {
+
+    const arr  = [`a`, `b`];
+    const coll = new Collection(ItemModel, arr);
+
+    coll.forEach(item => item.should.be.instanceOf(ItemModel));
 
   });
 
-  it(`instantiates with all items as instances of the provided model`);
+  it(`transforms items to instances of the provided model when added`, () => {
 
-  it(`transforms items to instances of the provided model when added`);
+    const coll = new Collection(ItemModel);
+    const obj  = {};
+
+    coll.push(obj);
+
+    const [item] = coll;
+
+    item.should.not.equal(obj);
+    item.should.be.instanceOf(ItemModel);
+
+  });
 
 });
